f42d047732e2879c01150556348eeaf88284b621
Ronak Sheth <ronak.sheth@sheridancollege.ca>
2023-05-16 14:45:58 -0400

    Added hello print

--- src/testprojs23tuepm/Testprojs23TuePM.java
+++ src/testprojs23tuepm/Testprojs23TuePM.java
@@ -15,6 +15,7 @@ public class Testprojs23TuePM {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("hello");
     }
     
 }
