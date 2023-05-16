70bab19cd03de31e0a9e96d56a509208ff2158b6
Ronak Sheth <ronak.sheth@sheridancollege.ca>
2023-05-16 14:47:09 -0400

    removing happy tuesday

--- src/testprojs23tuepm/Testprojs23TuePM.java
+++ src/testprojs23tuepm/Testprojs23TuePM.java
@@ -16,7 +16,6 @@ public class Testprojs23TuePM {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hello");
-        System.out.println("Happy tuesday");
     }
     
 }
